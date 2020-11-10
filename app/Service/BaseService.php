2cfddacd29675d3971f177c9ab3bead38b480f96
zyvincenthu <zyvincenthu@tencent.com>
2020-11-10 18:39:41 +0800

    异步队列服务封装基础类里面

--- app/Service/BaseService.php
+++ app/Service/BaseService.php
@@ -7,5 +7,13 @@ namespace App\Service;
 
 class BaseService
 {
+    /**
+     * @var \App\Service\JobDispatchService
+     */
+    protected $jobDispatcher;
 
+    public function __construct(JobDispatchService $jobDispatcher)
+    {
+        $this->jobDispatcher = $jobDispatcher;
+    }
 }
