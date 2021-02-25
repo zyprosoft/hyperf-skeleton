f6c83231785ab30f09b8fe408e8177238a39ce27
zyprosoft <1003081775@qq.com>
2021-02-25 19:36:28 +0800

    Update server.php

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -19,7 +19,7 @@ return [
             'name' => 'http',
             'type' => Server::SERVER_HTTP,
             'host' => '0.0.0.0',
-            'port' => 9506,
+            'port' => intval(env('HTTP_SERVER_PORT')),
             'sock_type' => SWOOLE_SOCK_TCP,
             'callbacks' => [
                 Event::ON_REQUEST => [Hyperf\HttpServer\Server::class, 'onRequest'],
